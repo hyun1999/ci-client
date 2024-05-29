94721053621e8466cc28ec977f06beafa9c9b862
parkhyunseo <dazzle1999@naver.com>
2024-05-29 17:06:48 +0900

    update

--- src/api/board.js
+++ src/api/board.js
@@ -1,11 +1,11 @@
 import { api } from "./config/network";
 
 export const createBoard = async (formData) => {
-  const res = await api("api/boards", "POST", formData);
+  const res = await api("/boards", "POST", formData);
   return res;
 };
 
 export const getBoard = async () => {
-  const res = await api("api/boards", "GET");
+  const res = await api("/boards", "GET");
   return res;
 };
--- src/api/config/network.js
+++ src/api/config/network.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = async (url, method, body) => {
-  axios.defaults.baseURL = "http://104.198.188.88:8080/api";
+  // axios.defaults.baseURL = "http://104.198.188.88:8080/api";
 
   const res = await axios({
     url,
